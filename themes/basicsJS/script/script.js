c60ba918327e717510ac5429573a710ec82a18a1
XXXmez <russian.bogd@gmail.com>
2021-08-14 13:44:04 +0300

    studied cycles

--- themes/basicsJS/script/script.js
+++ themes/basicsJS/script/script.js
@@ -258,3 +258,33 @@ console.log('');console.log('');
     }
 
 }
+
+console.log('');console.log('');
+// 14. Циклы
+{
+    console.log('14. Циклы');
+
+    let num = 50;
+    
+    /*while (num < 55) {
+        console.log(num);
+        num++;
+    }*/
+
+    do {
+        console.log(num);
+        num ++
+    }
+    while (num < 55);
+
+    for (let i = 1; i < 8; i++) {
+        if (i == 6) {
+            //break;        // прервали цикли когда он достиг нужного момента
+            continue;
+            
+        }
+        console.log(i);
+    }
+    
+
+}
