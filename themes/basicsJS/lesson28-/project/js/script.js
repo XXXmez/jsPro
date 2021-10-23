561c40728272efa04959dc7865a01b117013e130
XXXmez <russian.bogd@gmail.com>
2021-10-23 20:42:10 +0300

    Made the sorting. Brought out with numbering

--- themes/basicsJS/lesson28-/project/js/script.js
+++ themes/basicsJS/lesson28-/project/js/script.js
@@ -1,3 +1,64 @@
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+// ищем элементы нужные нам на странице
+const promoAdvImg = document.querySelectorAll('.promo__adv img'),
+      promoGenre = document.querySelector('.promo__genre'),
+      promoBg = document.querySelector('.promo__bg'),
+      promoInteractiveItem = document.querySelectorAll('.promo__interactive-item'),
+      promoInteractiveList = document.querySelector('.promo__interactive-list');
+
+// функция удаления переданных в нее массива или одного элемента
+let delAd = function(elem) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].remove();
+        };
+        console.log('больше 1');
+    } else if (elem.length == 1) {
+        elem.remove();
+        console.log('равно 1');
+    } else {
+        console.log('ошибка в переданном значении в функцию delAd');
+    }
+    
+};
+delAd(promoAdvImg);
+
+// заменяю текст
+promoGenre.textContent = 'драма';
+
+//заменяю картинку
+promoBg.style.background = 'url(../img/bg.jpg)';
+
+// удаляю старый список просмотренных фильмов
+for (let i = 0; i < promoInteractiveItem.length; i++) {
+    promoInteractiveItem[i].remove();
+}
+// сортировка
+movieDB.movies.sort();
+// вывожу новый список из массива
+for(let i = 0; i < movieDB.movies.length; i++) {
+    let liPII = document.createElement('li');
+    liPII.classList.add('promo__interactive-item');
+    let divDel = document.createElement('div');
+    divDel.classList.add('delete');
+    liPII.append(divDel);
+    liPII.textContent = `${i+1}: ${movieDB.movies[i]}`;
+    promoInteractiveList.append(liPII);
+}
+
+
+
 /* Задания на урок:
 
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
@@ -11,48 +72,3 @@
 Отсортировать их по алфавиту 
 
 5) Добавить нумерацию выведенных фильмов */
-
-'use strict';
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const promoAdvImg = document.querySelectorAll('.promo__adv img'),
-      promoGenre = document.querySelector('.promo__genre'),
-      promoBg = document.querySelector('.promo__bg'),
-      promoInteractiveItem = document.querySelectorAll('.promo__interactive-item'),
-      promoInteractiveList = document.querySelector('.promo__interactive-list');
-
-let delAd = function(elem) {
-    for (let i = 0; i < elem.length; i++) {
-        elem[i].remove();
-    };
-};
-delAd(promoAdvImg);
-
-promoGenre.textContent = 'драма';
-
-promoBg.style.background = 'url(../img/bg.jpg)';
-
-for (let i = 0; i < promoInteractiveItem.length; i++) {
-    promoInteractiveItem[i].remove();
-}
-
-
-for(let i = 0; i < movieDB.movies.length; i++) {
-    let liPII = document.createElement('li');
-    liPII.classList.add('promo__interactive-item');
-    let divDel = document.createElement('div');
-    divDel.classList.add('delete');
-    liPII.append(divDel);
-    liPII.textContent = movieDB.movies[i];
-    
-    promoInteractiveList.append(liPII);
-}
