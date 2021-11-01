3778cfa9f64c0123e98a070393d0867d3dee819f
XXXmez <russian.bogd@gmail.com>
2021-11-01 20:09:23 +0300

    Started redesigning the project using events.

--- themes/basicsJS/lesson28-/project/js/script.js
+++ themes/basicsJS/lesson28-/project/js/script.js
@@ -15,7 +15,9 @@ const promoAdvImg = document.querySelectorAll('.promo__adv img'),
       promoGenre = document.querySelector('.promo__genre'),
       promoBg = document.querySelector('.promo__bg'),
       promoInteractiveItem = document.querySelectorAll('.promo__interactive-item'),
-      promoInteractiveList = document.querySelector('.promo__interactive-list');
+      promoInteractiveList = document.querySelector('.promo__interactive-list'),
+      addingInput = document.querySelector('.adding__input'),
+      formAdd = document.querySelector('form.add');
 
 // функция удаления переданных в нее массива или одного элемента
 promoAdvImg.forEach((item) => {
@@ -41,17 +43,31 @@ movieDB.movies.forEach((film, i) => {
     `; 
 });
 
+formAdd.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let newFilf = addingInput.value
+    if (newFilf) {
+        movieDB.movies.push(newFilf);
+        movieDB.movies.sort();
+    }
+})
+
 
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту 
+
+*/
