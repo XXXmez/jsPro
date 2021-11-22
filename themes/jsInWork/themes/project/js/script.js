baa4fd6d3fa85b8d153bb5eecce53b17dfa57dcf
XXXmez <russian.bogd@gmail.com>
2021-11-23 01:30:51 +0300

    Implementation of the script for sending data to the server

--- themes/jsInWork/themes/project/js/script.js
+++ themes/jsInWork/themes/project/js/script.js
@@ -212,4 +212,57 @@ window.addEventListener('DOMContentLoaded', () => {
         '7.87',
         'menu__item'
     ).createMenuCart();
+
+
+    // forms
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Мы вам перезвоним!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const obj = {};
+            formData.forEach((v,k) => {
+                obj[k] = v;
+            });
+
+            const json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove()
+                    }, 2000)
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            })
+        });
+    }
 });
