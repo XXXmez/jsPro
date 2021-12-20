ba58411217765e42d8ff629437cf8eaffab2f716
XXXmez <russian.bogd@gmail.com>
2021-12-20 03:58:20 +0300

    Examined getters and setters in objects

---
+++ themes/advancedJS/themes/getSet/script/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Свойства акцессоры
+const persone = {
+    name: 'Ruslan',
+    age: 25,
+    get userAge() {
+        return this.age;
+    },
+    set userAge(num) {
+        this.age = num
+    }
+}
+
+console.log(persone.userAge = 30);
+console.log(persone);
