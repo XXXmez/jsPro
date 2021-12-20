1db714d78fc3eb30b3bc3b688c3089841e27f2db
XXXmez <russian.bogd@gmail.com>
2021-12-20 03:48:06 +0300

    Added saving statistics to localStorage

--- themes/jsInWork/themes/project/js/script.js
+++ themes/jsInWork/themes/project/js/script.js
@@ -559,9 +559,21 @@ window.addEventListener('DOMContentLoaded', () => {
     // calc calorie
 
     const resultCalc = document.querySelector('.calculating__result span');
-    let gender = 'female',
-        height, weight, age, 
+
+    let gender, height, weight, age, activity;
+
+    if (localStorage.getItem('gender')) {
+        gender = localStorage.getItem('gender');
+    } else {
+        gender = 'female';
+        localStorage.setItem('gender', gender)
+    }
+    if (localStorage.getItem('activity')) {
+        activity = localStorage.getItem('activity');
+    } else {
         activity = 1.375;
+        localStorage.setItem('activity', activity)
+    }
 
     function colorieCalc() {
         if (!gender || !height || !weight || !age || !activity) {
@@ -577,15 +589,17 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     colorieCalc();
 
-    function getStaticInfo(parentSelector, activeClass) {
-        const element = document.querySelectorAll(`${parentSelector} div`);
+    function getStaticInfo(selector, activeClass) {
+        const element = document.querySelectorAll(selector);
 
         element.forEach(elem => {
             elem.addEventListener('click', () => {
                 if (elem.getAttribute('data-ratio')) {
                     activity = elem.getAttribute('data-ratio');
+                    localStorage.setItem('activity', activity);
                 } else {
                     gender = elem.id;
+                    localStorage.setItem('gender', gender);
                 }
                 console.log(activity, gender);
                 element.forEach(e => e.classList.remove(activeClass));
@@ -600,23 +614,51 @@ window.addEventListener('DOMContentLoaded', () => {
         const input = document.querySelector(`${parentSelector}`);
 
         input.addEventListener('input', () => {
+            if (input.value.match(/\D/g)) {             // в данном случае можно еще так (isNaN(+input.value))
+                input.style.border = '2px solid red';
+            } else {
+                input.style.border = '';
+            }
             switch(input.getAttribute('id')) {
                 case 'height':
                     height = +input.value;
+                    localStorage.setItem('height', height);
                     break;
                 case 'weight':
                     weight = +input.value;
+                    localStorage.setItem('weight', weight);
                     break;
                 case 'age':
                     age = +input.value;
+                    localStorage.setItem('age', age);
                     break;
             }
             colorieCalc();
         });
     };
 
-    getStaticInfo('#gender', 'calculating__choose-item_active');
-    getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active');
+    function getInitLocalSet(selector, activeClass) {
+        const element = document.querySelectorAll(`${selector} div`);
+
+        element.forEach(e => {
+            e.classList.remove(activeClass);
+            if (e.getAttribute('data-ratio')) {
+                if (e.getAttribute('data-ratio') == localStorage.getItem('activity')) {
+                    e.classList.add(activeClass);
+                }
+            } else {
+                if (e.id == localStorage.getItem('gender')) {
+                    e.classList.add(activeClass);
+                }
+            }
+        })
+    };
+
+    getInitLocalSet('#gender', 'calculating__choose-item_active');
+    getInitLocalSet('.calculating__choose_big', 'calculating__choose-item_active');
+
+    getStaticInfo('#gender div', 'calculating__choose-item_active');
+    getStaticInfo('.calculating__choose_big div', 'calculating__choose-item_active');
     getDinamicInfo('#height');
     getDinamicInfo('#weight');
     getDinamicInfo('#age');
